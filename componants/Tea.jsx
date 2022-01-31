9ff070fef8406499e85c2afc6d4cf22293b97012
oussama <oussamaalouacheedv@gmail.com>
2022-01-31 14:34:30 +0100

    show

--- componants/Tea.jsx
+++ componants/Tea.jsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/Gateaux.module.css";
 
 const Gateux = ({Gateau}) => {
   return (
     <div className={styles.container}>
+      <Link href={`/product/${Gateau._id}`} passHref>
       <Image src={Gateau.img} alt="" width="500" height="500" />
+
+      </Link>
       <h1 className={styles.title}>{Gateau.title} </h1>
       <span className={styles.price}>{ Gateau.prices[0]}</span>
       <p className={styles.desc}>
